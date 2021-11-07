7e08a1731ed397290fd29e961524c2ff60402138
austin <austinchang4@gmail.com>
2021-11-07 23:25:56 +0800

    use spacy as the tokeniser

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import json
 from flask import Flask, request
 import cld3
+import spacy
+
+en = spacy.load("en_core_web_lg")
+zh = spacy.load("zh_core_web_lg")
 
 app = Flask(__name__)
 
@@ -16,6 +20,23 @@ def langdet():
             lang = cld3.get_language(text)
             output.append(lang.language)
 
+        return json.dumps(output)
+    except Exception as e:
+        return json.dumps({'error': f'Error: {e}'}), 500
+
+
+@app.route("/api/tokenise", methods=['POST'])
+def tokenise():
+    try:
+        # todo
+        texts = request.get_json(force=True)
+        output = []
+
+        for text in texts:
+            if text["lang"] == "en":
+                tokens = en.tokenizer(text["text"])
+                output.append(sorted([{"token": token.orth_, "score": token.rank} for token in tokens if not token.is_space and
+                              not token.is_punct and len(token.orth_) >= 3 and (token.orth_ not in en.Defaults.stop_words)], key=lambda x: x["score"], reverse=True))
         return json.dumps(output)
     except Exception as e:
         return json.dumps({'error': f'Error: {e}'}), 500
